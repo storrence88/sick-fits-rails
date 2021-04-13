59f156755f24bb8e7c2fca81c07aeb4d1e4852e0
Steven Torrence <storrence88@gmail.com>
2021-04-13 16:42:27 -0500

    Add rspec test for enumerization

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -5,6 +5,7 @@ require 'rails_helper'
 RSpec.describe Product, type: :model do
   it { is_expected.to belong_to(:user) }
 
+  it { is_expected.to enumerize(:status) }
   it { is_expected.to validate_presence_of(:name) }
   it { is_expected.to validate_presence_of(:description) }
   it { is_expected.to validate_presence_of(:price) }
